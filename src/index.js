bc9a052814148fab77bd33bc7fb3bc941ccf3b9f
Lavanya <Laavs03@gmail.com>
2024-08-17 16:56:14 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import React,{useState,useEffect} from 'react';
+function Userdemo(){
+    const[users,setUsers]=useState([]);
+    const[loading,setLoading]=useState(true);
 
-function Userdemo() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.log("Error message:", error);
-        setLoading(false);
-      });
-  },[]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>This is the list</h1>
-      <ul style={{listStyleType:'number'}}>
-        {users.map(user => (
-          <li key={user.id}>{user.name},{user.email}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data =>{
+            setUsers(data);
+            setLoading(false);
+        })
+        .catch(error =>{
+            console.error("if it is not loading,will be error");
+            setLoading(false);
+        })
+    },[]);
+    if(loading){
+        return <p>Loading...</p>
+    }
+    return(
+        <div>
+            <h1>List out the users in API</h1>
+            <ul>
+                {users.map(user => (
+                    <li key = {user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
-
-ReactDOM.render(<Userdemo />, document.getElementById('root'));
-
+ReactDOM.render(<Userdemo/>,document.getElementById("root"));
